70ded2bd9e1d7e1dc47c4f78d4fb53fe87c5e03f
AsfaQasim <asfaqasim144@gmail.com>
2024-12-06 09:13:07 -0800

    asfa

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='w-full min-h-[70vh] px-16 py-20'>
+        <h1>Home/Contact</h1>
+      <Image
+        src = {"/call.png"}
+        alt = "call"
+       width={135}
+       height={323}
+      
+
+
+      />
+      </div>
+    </div>
   )
 }
 
